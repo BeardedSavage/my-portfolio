6cb6a61de1a30970b8c50566b669ed040c7ab99a
BeardedSavage <dangelidakis2015@gmail.com>
2025-02-16 17:06:52 -0600

    Changed script to use nodemon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 8080;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
